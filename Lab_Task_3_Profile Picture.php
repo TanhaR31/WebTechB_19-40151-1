dad5f620895b3cf981ce1581be821a4d489955ad
Tanha Reja <77929567+TanhaR31@users.noreply.github.com>
2021-03-03 00:24:57 +0600

    Add files via upload

---
+++ Lab_Task_3_Profile Picture.php
@@ -0,0 +1,24 @@
+
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+
+<form action="Lab_Task_3_upload.php" method="post" enctype="multipart/form-data">
+  <fieldset>
+    <legend>Profile Picture</legend>
+    <img src="Lab_Task_3_Screenshot 2021-03-02 203256.png" width="100" height="100"><br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <hr>
+    <input type="submit" name="submit" value="Submit">
+  </fieldset>
+</form>
+</form>
+
+
+  
+  </body>
+  </html>
