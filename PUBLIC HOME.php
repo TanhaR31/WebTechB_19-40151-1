893242d44b12a21f4b7d2f21ab36d124ac3b921e
Tanha Reja <77929567+TanhaR31@users.noreply.github.com>
2021-03-17 23:31:01 +0600

    A. Public Home

---
+++ PUBLIC HOME.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <title>xCompany</title>
+</head>
+<body> 
+<h1 ><img src="companylogo.jpg" alt="company" width="100" height="50"></h1>
+<table align="right">
+<tr>
+<td><a href="PUBLIC HOME.php"> Home |</a></td>
+<td><a href="LOGIN.php"> Login |</a></td>
+<td><a href="REGISTRATION.php"> Registration </a></td>
+</tr>
+<?php
+echo "Welcome to xCompany";
+?>
+</table>
+<?php include 'footer.php';?>
+ </body>
+</html>
