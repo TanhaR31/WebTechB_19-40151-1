b8124edc74f7298f1d00a536a75516fb0dd62d33
Tanha Reja <77929567+TanhaR31@users.noreply.github.com>
2021-03-17 23:40:31 +0600

    F. View Profile

---
+++ VIEW PROFILE.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <title>Profile</title>
+  <h1 ><img src="companylogo.jpg" alt="company" width="100" height="50"></h1>
+</head>
+<body>
+<table align="right">
+	<tr>
+    <td> Logged in <a href="VIEW PROFILE.php"> bob |</a></td>
+    <td><a href="LOGOUT.php"> Logout </a></td>
+    </tr>
+</table>
+
+	<h2>PROFILE</h2>
+	<img src="login logo.jpg" alt="logo" width="100" height="100"><br><br>
+	<a href="PROFILE PICTURE"> change </a>
+	<p>Name: Bob</p>
+	<p>Email: bob@aiub.edu</p>
+	<p>Gender: Male</p>
+	<p>Date of Birth: 19/09/1998</p><br>
+	<a href="EDIT PROFILE.php"> Edit Profile </a>
+
+
+<br><br>
+
+<p><a href="LOGGED IN DASHBOARD.php"> Dashboard </a></p>
+<p><a href="VIEW PROFILE.php"> View Profile </a></p>
+<p><a href="EDIT PROFILE.php"> Edit Profile </a></p>
+<p><a href="CHANGE PROFILE PICTURE.php"> Change Profile Picture </a></p>
+<p><a href="CHANGE PASSWORD.php"> Change Password </a></p>
+<p><a href="LOGOUT.php"> Logout </a></p>
+<?php include 'footer.php';?>
+ </body>
+</html>
