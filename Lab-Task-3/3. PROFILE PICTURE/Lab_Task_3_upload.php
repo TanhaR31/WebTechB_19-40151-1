dc8e81e6604aacb687accea96e6387aba15e3391
Tanha Reja <77929567+TanhaR31@users.noreply.github.com>
2021-04-08 02:19:35 +0600

    Lab_Task_3

---
+++ Lab-Task-3/3. PROFILE PICTURE/Lab_Task_3_upload.php
@@ -0,0 +1,26 @@
+<?php
+$target_dir = "Lab_Task_3_uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+  echo "Picture format must be in jpeg or jpg or png"; echo "<br>";
+  $uploadOk = 0;
+}
+
+if ($_FILES["fileToUpload"]["size"] > 400000) {
+  echo "Picture size should not be more than 4MB"; echo "<br>";
+  $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded."; echo "<br>";
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file."; echo "<br>";
+  }
+}
+?>
