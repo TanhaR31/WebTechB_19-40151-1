24f14fb8a7448b9400935a52dc39de011eebaef3
Tanha Reja <77929567+TanhaR31@users.noreply.github.com>
2021-04-28 11:06:34 +0600

    Lab_Task_8

---
+++ Lab-Task-8/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="CSS/form_style.css">
+  <link rel = "icon" href = "https://icons.iconarchive.com/icons/mag1cwind0w/akisame/128/Leaf-icon.png">
+</head>
+<body>
+
+<div class="wrapper">
+  <div class="row">
+    <div class="column">
+      <table>
+        <?php require 'Controller/PUBLIC USER.php' ?>
+      </table>
+    </div>
+    <div class="column">
+      <table>
+        <?php require 'Controller/WELCOME USER.php' ?>
+      </table>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
